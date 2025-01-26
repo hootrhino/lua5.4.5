14c21f083847cb4302bd49dc8ba98e28b40718e5
wwhai <cnwwhai@gmail.com>
2025-01-26 16:36:12 +0800

    dev: remove unused libs

--- linit.c
+++ linit.c
@@ -38,14 +38,9 @@
 */
 static const luaL_Reg loadedlibs[] = {
     {LUA_GNAME, luaopen_base},
-    {LUA_LOADLIBNAME, luaopen_package},
-    {LUA_COLIBNAME, luaopen_coroutine},
     {LUA_TABLIBNAME, luaopen_table},
-    {LUA_IOLIBNAME, luaopen_io},
     {LUA_STRLIBNAME, luaopen_string},
     {LUA_MATHLIBNAME, luaopen_math},
-    {LUA_UTF8LIBNAME, luaopen_utf8},
-    {LUA_DBLIBNAME, luaopen_debug},
     {NULL, NULL}};
 
 LUALIB_API void luaL_openlibs(lua_State *L)
